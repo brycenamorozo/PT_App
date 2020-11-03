300b77da3a062d8947407e4229581bc2810945e4
reyanlee <anni3lee03@gmail.com>
2020-11-03 13:13:45 -1000

    calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -2,15 +2,20 @@
   <div>
     <Navbar/>
     <h1 class="ma-3">This is a calendar page</h1>
+    <Calendar/>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
+import Calendar from '../components/Calendar'
 
 export default {
     name: 'Calendar',
-    components: { Navbar },
+    components: { 
+      Navbar,
+      Calendar
+   },
 
   data: () => ({
     //
