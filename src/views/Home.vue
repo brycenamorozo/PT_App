4990e5d2b2e6068e2b2b92d4a1da5d70f0f1f3f3
reyanlee <anni3lee03@gmail.com>
2020-12-06 20:44:30 -1000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,7 +33,6 @@ export default {
             items: [{
                     title: 'Neck',
                     route: '/incomplete',
-
                 },
                 {
                     title: 'Arm',
