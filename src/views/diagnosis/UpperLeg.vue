cced4ac2637faef13fabd772f38542ed2e4edf93
reyanlee <anni3lee03@gmail.com>
2020-12-15 19:36:43 -1000

    upper and lower leg cards

--- src/views/diagnosis/UpperLeg.vue
+++ src/views/diagnosis/UpperLeg.vue
@@ -4,17 +4,28 @@
   <div class="pa-7">
     <h1>Step 1: Self Diagnosis</h1>
     <h3 class="mt-3">Select specific area of pain</h3>
-    <v-card class="mt-3" elevation="12" width="256">
-      <v-navigation-drawer floating permanent>
-        <v-list dense rounded>
-          <v-list-item v-for="item in items" :key="item.title" :to="item.route">
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-card>
+    <v-container class="pa-10">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 xlg2 v-for="item in items" :key="item.title">
+          <v-card  class="text-center ma-3 pt-3 px-3 d-flex flex-column">
+            <v-card-text>
+              <h2>{{ item.title }}</h2>
+            </v-card-text>
+
+            <v-responsive class="pt-4">
+              <v-img :aspect-ratio="7/9"  :src="item.picture"></v-img>
+            </v-responsive>
+
+            <v-card-actions :to="item.route">
+              <v-spacer></v-spacer>
+              <v-btn flat depressed color="white grey--text" :to="item.route">
+                <v-icon large right>mdi-arrow-right</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex> 
+      </v-layout>
+    </v-container>
   </div>
 </div>
 </template>
@@ -29,30 +40,12 @@ export default {
   data() {
     return {
       items: [
-        {
-          title: 'Gluteal muscles',
-          route: '/incomplete',
-        },
-        {
-          title: 'Hamstring',
-          route: '/incomplete',
-        },
-        {
-          title: 'Psoas',
-          route: '/incomplete',
-        },
-        {
-          title: 'Groin',
-          route: '/incomplete',
-        },
-        {
-          title: 'Quads',
-          route: '/incomplete',
-        },
-        {
-          title: 'I\'m not sure',
-          route: '/incomplete',
-        }
+        { title: 'Gluteal muscles', route: '/incomplete', picture: require('../../assets/GlutealMuscles.jpg') },
+        { title: 'Hamstring', route: '/incomplete', picture: require('../../assets/Hamstring.jpg') },
+        { title: 'Psoas', route: '/incomplete', picture: require('../../assets/Psoas.jpg') },
+        { title: 'Groin', route: '/incomplete', picture: require('../../assets/Groin.jpg') },
+        { title: 'Quads', route: '/incomplete', picture: require('../../assets/Quads.jpg') },
+        { title: 'I\'m not sure', route: '/incomplete', picture: require('../../assets/UpperLeg.png') },
       ],
     }
   },
