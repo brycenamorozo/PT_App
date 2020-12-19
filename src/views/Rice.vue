700bc6658b6d3a10e5ceae5be42674d443d173f2
brycenamorozo <Brycenamor808@gmail.com>
2020-12-18 22:34:59 -1000

    Update Rice.vue

--- src/views/Rice.vue
+++ src/views/Rice.vue
@@ -2,8 +2,36 @@
 <div>
   <Navbar/>
   <div class="pa-7">
-    <h1>RICE</h1>
-    <h3 class="mt-3">Learn about the importance of resting your body before trying these exercises.</h3>
+    <h1>RICE:</h1>
+    <h2 class="mt-3"> An acronym used to remember how to treat a minor muscle injury stands for rest, ice, compress, and elevate. Learn about the importance of resting your body before trying these exercises.</h2>
+    <h2 class="mt-5">Rest</h2>
+    <p>Resting is one of the most effective ways to start your healing process. Your injured muscle will be weak and vulnerable to further injury, especially in the first few hours. Take a break from moving it to help it heal.</p>
+    <h2 class="mt-5">Ice</h2>
+    <p>The benefits of applying ice are greatest within the first day or two after sustaining an injury. Apply a bag of crushed ice, a bag of frozen veggies, or an ice pack to your injury. It will help relieve pain and prevent swelling by decreasing blood flow to the area. To avoid frostbite, never place the ice directly on your bare skin. Instead, wrap it in a thin cloth or towel before applying it to the injured area. Apply ice for 15 to 20 minutes at a time, and allow your skin to return to normal temperature in between icing.</p>
+    <h2 class="mt-5">Compress</h2>
+    <p>An elastic bandage wrapped firmly around your injury can help minimize swelling by preventing the buildup of fluid. It can also help ease pain by keeping the injured area somewhat immobilized. The bandage may not be enough to immobilize the injured area entirely, but it will provide some support and remind you to keep it still.</p>
+    <p>If the bandage causes tingling or numbness, remove it and rewrap it more loosely. It shouldn’t be so tight that it causes discomfort or interferes with your blood flow. Even gentle compression can help keep fluid from collecting around the injury.</p>
+    <h2 class="mt-5">Elevate</h2>
+    <p>Elevating an injury above the level of your heart will helping minimize swelling by allowing fluid to drain away from the area. If you can’t raise it above your heart, try to keep the injured area at the same level as your heart or close to it. If you suffered an injury to your buttocks or hips, try lying down with a pillow or two wedged under your buttocks and lower back to help lift it.</p>
+    <h2 class="mt-5">One day after injury</h2>
+    <p>The day after suffering an injury is often the most painful. Swelling will likely be at its worst a few hours to a couple of days after your injury occurred. Bruising will continue to develop for the first few hours and may be very noticeable the next day. It may turn the entire area a deep purple or black color.</p>
+    <p>Continue using the R.I.C.E. treatment method for the first 48 to 72 hours after your injury. During this time, you should keep the injured area wrapped with an elastic bandage, elevate it when you can, and apply ice every few hours. If you’re experiencing pain, consider taking an over-the-counter anti-inflammatory painkiller, such as aspirin or ibuprofen. Acetaminophen can also help relieve pain, but it won’t reduce inflammation.</p>
+    <p>During the first three days following a sports injury, don’t apply heat to the area. While it may feel soothing, heat can increase circulation and worsen swelling.</p>
+    <h2 class="mt-5">Three days after injury</h2>
+    <p>Musculoskeletal injuries usually begin to heal within 72 hours. By the third day, your pain, swelling, and bruising will likely begin to subside. The changing colors of your bruises reflect the breakdown of blood that’s been trapped in your skin as a result of your injury</p>
+    <p>Once your swelling has gone down, you can begin to alternate heat packs with ice. Applying heat will promote the circulation of blood to the injured area, helping to deliver oxygen and nutrients to support the healing process. Both heat and ice can help ease pain, and many trainers recommend alternating them every few hours.</p>
+    <p>When your swelling has gone down, you can also remove your compression bandage and begin to gently exercise the injured area. Start slowly by lightly stretching the area, never pushing it to the point of pain. You’ll notice your range of motion increasing a little more each day. Keep stretching and moving for the first few weeks until you’re comfortable with normal use and exercise.</p>
+    <p>Immobilizing injuries for too long can cause stiffness and loss of strength. You want to start moving the injured area, or walking with a normal gait, as soon as possible.</p>
+    <h2 class="mt-5">When to see a doctor</h2>
+    <p>If you suspect your injury is severe, make an appointment with your doctor or go to the emergency department. The following symptoms may be a sign of a severe injury that requires professional care:</p>
+    <p>Severe swelling</p>
+    <p>Visible bumps or limbs bent at strange angles</p>
+    <p>Popping or crunching in injured area</p>
+    <p>Instability in joint</p>
+    <p>Trouble breathing</p>
+    <p>Dizziness</p>
+    <p>Fever</p>
+    <p>You should also contact your doctor if you have an injury that seems minor but doesn’t improve with home treatment. After the first month, you should have no more swelling or visible bruising around the injured area. If you notice swelling or discoloration after four weeks, make an appointment with your doctor. Severe pain after the first few weeks is also a good reason to visit them.</p>
   </div>
 </div>
 </template>
